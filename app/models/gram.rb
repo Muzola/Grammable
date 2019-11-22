75e0ac069f01103d7381cb85f3fb8de5b81030e6
Muzola <amazzolaphoto@gmail.com>
2019-11-22 19:38:16 +0000

    added image validation and fixed tests

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,8 +1,9 @@
 class Gram < ApplicationRecord
   validates :message, presence: true
-
+  validates :picture, presence: true
+  mount_uploader :picture, PictureUploader
   belongs_to :user
   has_many :pictures
-  mount_uploader :picture, PictureUploader
+ 
 
 end
